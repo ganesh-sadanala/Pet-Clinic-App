78c9d94f0676d237043a2fdbb1b0f4b8836cfffb
gsadanala <gsadanala@paypal.com>
2021-03-08 19:21:31 +0530

    Load Bootstrap Data on Startup. Closes #14

--- pet-clinic-data/src/main/java/com/ganesh/petclinicapp/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/ganesh/petclinicapp/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.ganesh.petclinicapp.services.map;
 
 import com.ganesh.petclinicapp.model.Pet;
-import com.ganesh.petclinicapp.services.CrudService;
+import com.ganesh.petclinicapp.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/com/ganesh/petclinicapp/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/ganesh/petclinicapp/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.ganesh.petclinicapp.services.map;
 
 import com.ganesh.petclinicapp.model.Vet;
-import com.ganesh.petclinicapp.services.CrudService;
+import com.ganesh.petclinicapp.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
