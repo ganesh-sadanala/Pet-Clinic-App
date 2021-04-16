b4b4088351ac4ebf1ea645e67db3ac95116b1af5
gsadanala <gsadanala@paypal.com>
2021-04-16 15:26:38 +0530

    Create Vet Speciality Entity, Associate to Vet #27

--- pet-clinic-data/src/main/java/com/ganesh/petclinicapp/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ganesh/petclinicapp/model/Vet.java
@@ -1,4 +1,13 @@
 package com.ganesh.petclinicapp.model;
 
 public class Vet extends Person{
+    private Speciality specialities;
+
+    public Speciality getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Speciality specialities) {
+        this.specialities = specialities;
+    }
 }
